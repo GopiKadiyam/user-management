12d359cdcc78a4ea5a5ae502dd0eeaf109c1782f
GopiKadiyam <learnergopikrishna@gmail.com>
2023-01-18 19:19:00 +0530

    initial commit

---
+++ src/test/java/com/gk/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
